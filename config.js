a3a2b5341419d3f09f4d1096be3a1a542d4d1dae
VeraOgai2021 <77864101+VeraOgai2021@users.noreply.github.com>
2022-03-18 12:41:20 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,14 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAbPPLZ9I2leRy-RYbP-_ZIaBTxA8NwRmo",
+  authDomain: "class-test-cbec9.firebaseapp.com",
+  databaseURL: "https://class-test-cbec9-default-rtdb.firebaseio.com",
+  projectId: "class-test-cbec9",
+  storageBucket: "class-test-cbec9.appspot.com",
+  messagingSenderId: "809329346083",
+  appId: "1:809329346083:web:d2dda3c21229ee66401396"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
